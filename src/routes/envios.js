d456be8092fdd62d1ee30e590442824ca5a230a2
Leonardo Ortiz <leonardo_h_ortiz@hotmail.com>
2026-02-03 09:23:38 -0300

    fix duplicate entregar route

--- src/routes/envios.js
+++ src/routes/envios.js
@@ -5,13 +5,12 @@ const {
   crearEnvioPrePago,
   entregarEnvio,
   getEnviosEntregados,
-  marcarEnvioEntregado,
   getEnviosPendientes
 } = require("../controllers/enviosController");
 
 router.post("/prepago", crearEnvioPrePago);
 router.get("/pendientes", getEnviosPendientes);
-router.put("/:id/entregar", marcarEnvioEntregado);
+
 router.put("/:id/entregar", entregarEnvio);
 router.get("/entregados", getEnviosEntregados);
 
