9544dc93710fb41ee54dc4f98d7e5e357284d350
Leonardo Ortiz <leonardo_h_ortiz@hotmail.com>
2026-02-03 23:17:51 -0300

    debug database_url

--- src/db.js
+++ src/db.js
@@ -1,16 +1,14 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
+console.log("DATABASE_URL =", process.env.DATABASE_URL);
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  connectionString: process.env.DATABASE_URL
 });
 
-// Test conexión
-pool.query('SELECT 1')
-  .then(() => console.log('✅ PostgreSQL conectado correctamente'))
-  .catch(err => console.error('❌ Error PostgreSQL:', err));
+pool.query('select 1')
+  .then(() => console.log("✅ DB OK"))
+  .catch(err => console.error("❌ DB ERROR:", err));
 
 module.exports = pool;
