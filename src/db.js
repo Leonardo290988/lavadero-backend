9f02b731609809e89f6b5c30368f3ea73d26e91f
Leonardo Ortiz <leonardo_h_ortiz@hotmail.com>
2026-02-03 22:29:52 -0300

    Use  DATABASE_URL only for postgres

--- src/db.js
+++ src/db.js
@@ -1,16 +1,12 @@
-require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: true
 });
 
-// Test conexión
 pool.query('SELECT 1')
-  .then(() => console.log('✅ PostgreSQL conectado correctamente'))
-  .catch(err => console.error('❌ Error PostgreSQL:', err));
+  .then(() => console.log('✅ PostgreSQL conectado'))
+  .catch(err => console.error('❌ PostgreSQL error:', err));
 
 module.exports = pool;
