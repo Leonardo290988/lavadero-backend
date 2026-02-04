78448e523538705c4443f109f07cba0c430d0732
Leonardo Ortiz <leonardo_h_ortiz@hotmail.com>
2026-02-04 02:04:08 -0300

    url en db

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: "postgresql://${{Postgres.PGUSER}}:${{Postgres.PGPASSWORD}}@${{Postgres.PGHOST}}:${{Postgres.PGPORT}}/${{Postgres.PGDATABASE}}",
   ssl: { rejectUnauthorized: false }
 });
 
