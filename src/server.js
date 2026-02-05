d645ac635b44a57886cd28927de9ed64e615a9cd
Leonardo Ortiz <leonardo_h_ortiz@hotmail.com>
2026-02-05 02:16:47 -0300

    fix cors preflight

--- src/server.js
+++ src/server.js
@@ -6,8 +6,8 @@ const path = require("path");
 
 app.use("/pdf", express.static(path.join(__dirname, "pdf")));
 
-
-app.use(cors({origin: "*"}));
+app.use(cors({ origin: "*" }));
+app.options("*", cors());   // ✅ PRE-FLIGHT CORS FIX
 app.use(express.json());
 
 console.log('🔥🔥🔥 ESTE SERVER ES EL NUEVO 🔥🔥🔥');
@@ -17,26 +17,21 @@ const dashboardRoutes = require('./routes/dashboard');
 const clientesRoutes = require('./routes/clientes');
 const serviciosRoutes = require('./routes/servicios');
 const ordenesRoutes  = require('./routes/ordenes');
-const cajaRoutes     = require('./routes/caja'); // ✅ AGREGADO
+const cajaRoutes     = require('./routes/caja');
 const retirosRoutes = require('./routes/retiros');
 const pagosRoutes = require("./routes/pagos");
 const webhookRoutes = require("./routes/webhook");
 const enviosRoutes = require("./routes/envios");
 
-
-
-
 app.use('/api/dashboard', dashboardRoutes);
 app.use('/clientes', clientesRoutes);
 app.use('/servicios', serviciosRoutes);
 app.use('/ordenes', ordenesRoutes);
-app.use('/caja', cajaRoutes); // ✅ AGREGADO
+app.use('/caja', cajaRoutes);
 app.use('/retiros', retirosRoutes);
 app.use("/pagos", pagosRoutes);
 app.use("/webhook", webhookRoutes);
 app.use("/envios", enviosRoutes);
-
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
