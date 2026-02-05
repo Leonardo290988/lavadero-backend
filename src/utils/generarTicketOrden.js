b2e05bd77b30bc0bb63d1321855cac158797a9ca
Leonardo Ortiz <leonardo_h_ortiz@hotmail.com>
2026-02-05 20:12:35 -0300

    arreglo hora

--- src/utils/generarTicketOrden.js
+++ src/utils/generarTicketOrden.js
@@ -1,6 +1,6 @@
-import { fechaArgentina } from "../utils/fecha.js";
-const fs = require("fs");
-const path = require("path");
+import fs from "fs";
+import path from "path";
+import { fechaArgentina } from "./fecha.js";
 const PDFDocument = require("pdfkit");
 
 const generarTicketOrden = async (orden) => {
