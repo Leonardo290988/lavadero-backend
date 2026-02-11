68c8b57077e485524190d443f99e8b5a6a712f1b
Leonardo Ortiz <leonardo_h_ortiz@hotmail.com>
2026-02-10 22:20:17 -0300

    t

--- src/config/mercadopago.js
+++ src/config/mercadopago.js
@@ -1,4 +1,5 @@
 const { MercadoPagoConfig } = require("mercadopago");
+console.log("🧪 MP_ACCESS_TOKEN =", process.env.MP_ACCESS_TOKEN);
 
 const client = new MercadoPagoConfig({
   accessToken: process.env.MP_ACCESS_TOKEN
