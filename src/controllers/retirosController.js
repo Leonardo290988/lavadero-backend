d271f5a6c511a5b9cf6e425f1a978c288eca2338
Leonardo Ortiz <leonardo_h_ortiz@hotmail.com>
2026-02-11 21:46:51 -0300

    ticket

--- src/controllers/retirosController.js
+++ src/controllers/retirosController.js
@@ -240,7 +240,7 @@ const archivo = await generarTicketProvisorio({
       ok:true,
       orden_id: orden.id,
       tiene_envio: tieneEnvio,
-      pdf: `/pdf/provisorios/${archivoArchivo}`
+      pdf: `/pdf/provisorios/${nombreArchivo}`
     });
 
   } catch (error) {
